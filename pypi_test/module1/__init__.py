7fccbe8fe49c75ed939914a8d451e704c151d03c
zhangll <524631266@qq.com>
2018-09-29 00:20:24 +0800

    添加交互式命令的扩展使用,比如在脚本中添加hello 1

--- pypi_test/module1/__init__.py
+++ pypi_test/module1/__init__.py
@@ -3,6 +3,16 @@ import cmd
 import subprocess
 
 class CLI(cmd.Cmd):
+    """
+        Cmd 使用方法
+        比如想要在交互环境中输入 hello arg1 这样的指令
+        只要在继承了Cmd的类中定义 do_hello就可以了
+        方式为
+            def do_hello1():
+                print("hello")
+
+            eval('do_'+'hello1')()
+    """
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = 'zhangll test>'
@@ -11,6 +21,9 @@ class CLI(cmd.Cmd):
         print(">>>",arg)
         sub_cmd = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
         print(sub_cmd.communicate()[0])
+    def do_hello(self,arg1):
+        print(">>>>","welcome"+ arg1)
+
 def runcmd():
     cli = CLI()
     cli.cmdloop()
