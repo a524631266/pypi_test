8be0cfe200457c152612a38c3d2ca0167c667fe0
zhangll <524631266@qq.com>
2018-09-29 00:47:48 +0800

    动态添加类

--- pypi_test/module1/__init__.py
+++ pypi_test/module1/__init__.py
@@ -27,3 +27,30 @@ class CLI(cmd.Cmd):
 def runcmd():
     cli = CLI()
     cli.cmdloop()
+
+# a ='''
+# class Person(object):
+#     def __init__(self,name):
+#         self.name = name
+#     @property
+#     def name1(self):
+#         return self.name
+# '''
+
+# class obj(object): 
+
+#       pass 
+
+# create_obj = compile("obj()", 'Person.py', 'eval')
+# print(create_obj)
+# a  = eval(create_obj)
+
+
+
+# p1 = Person("zhangll")
+# print(p1.name1)
+
+import sys
+sys.path.append('/home/zhangll/github/pypi_test/pypi_test')
+module = __import__("module3")
+print(module)
--- pypi_test/module3.py
+++ pypi_test/module3.py
@@ -2,3 +2,7 @@ from pypi_test.module1 import runcmd
 
 def runcmd1():
     runcmd()
+def hello():
+    print("hello")
+class module():
+    name = 2222
